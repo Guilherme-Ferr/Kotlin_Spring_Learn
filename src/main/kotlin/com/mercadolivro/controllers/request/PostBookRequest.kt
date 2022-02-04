5742e79bd0fe6bad73e3df69d15aac46650933de
Guilherme-Ferr <guilhermefe007@hotmail.com>
2022-02-04 18:44:00 -0300

    commit tests

--- src/main/kotlin/com/mercadolivro/controllers/request/PostBookRequest.kt
+++ src/main/kotlin/com/mercadolivro/controllers/request/PostBookRequest.kt
@@ -4,10 +4,8 @@ import com.fasterxml.jackson.annotation.JsonAlias
 import java.math.BigDecimal
 
 data class PostBookRequest (
-    var name: String,
-
-    var price: BigDecimal,
-
     @JsonAlias("customer_id")
-    var customerId: Int
+    var customerId: Int,
+    var name: String,
+    var price: BigDecimal,
 )
